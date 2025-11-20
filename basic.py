7fb9ae673a3690529d52d5b7a57da5401850bbd7
Dante <dante@gmail.com>
2025-11-20 06:21:30 -0500

    m

--- basic.py
+++ basic.py
@@ -9,7 +9,7 @@ import base64
 
 load_dotenv()
 
-api_key=st.secrets["GOOGLE_API_KEY"]
+api_key=st.secrets["GOOGLE_API_KEY1"]
 client = genai.Client(api_key=api_key) 
 
 for model in client.models.list():
