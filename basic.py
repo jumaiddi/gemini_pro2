00a9cde41fb12053482a8bd8198dc55c83418dc4
Dante <dante@gmail.com>
2025-11-20 06:42:31 -0500

    m

--- basic.py
+++ basic.py
@@ -9,8 +9,8 @@ import base64
 
 load_dotenv() 
 
-api_key=st.secrets["GOOGLE_API_KEY1"]
-api_key1=st.secrets["GOOGLE_API_KEY"]
+api_key=st.secrets["GOOGLE_API_KEY"]
+api_key1=st.secrets["GOOGLE_API_KEY1"]
 api_key2=st.secrets["GOOGLE_API_KEY2"]
 client = genai.Client(api_key=api_key) 
 
